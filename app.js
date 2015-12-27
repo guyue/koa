a69a1a53eff49ab472c2bcc810e124c813b8ec8f
GuYue <guyue.jiwei@gmail.com>
2015-12-27 12:07:50 +0800

    add koa-onerror

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const config = require('./config/config');
 
 const app = koa();
 
+const onerror = require('koa-onerror');
+onerror(app);
+
 app.use(function *() {
     this.body = JSON.stringify(config);
 });
+
 
 app.listen(config.port);
