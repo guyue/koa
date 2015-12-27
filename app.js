ea219d6cc74c4f875cea992743ede99f86d53ab0
GuYue <guyue.jiwei@gmail.com>
2015-12-27 13:14:38 +0800

    add koa-generate-session

--- app.js
+++ app.js
@@ -4,13 +4,46 @@ const koa = require('koa');
 const config = require('./config/config');
 
 const app = koa();
+app.keys = ['study koa'];
 
 const onerror = require('koa-onerror');
 onerror(app);
 
+const session = require('koa-generic-session');
+
+app.use(session(app));
+
 app.use(function *() {
-    this.body = JSON.stringify(config);
+    switch(this.path) {
+        case '/get':
+            get.call(this);
+            break;
+        case '/remove':
+            remove.call(this);
+            break;
+        case '/regenerate':
+            yield regenerate.call(this);
+            break;
+        default:
+            get.call(this);
+    }
 });
 
+function get() {
+    const session = this.session;
+    session.count = session.count || 0;
+    session.count += 1;
+    this.body = session.count;
+}
+
+function remove() {
+    this.session = null;
+    this.body = 0;
+}
+
+function *regenerate() {
+    yield this.regenerateSession();
+    get.call(this);
+}
 
 app.listen(config.port);
