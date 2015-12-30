13e6ed67815621e8e1911910a632d72e2fc2b61d
GuYue <guyue.jiwei@gmail.com>
2015-12-30 18:54:12 +0800

    add meta

--- view/layout.jsx
+++ view/layout.jsx
@@ -12,6 +12,9 @@ const Layout = React.createClass({
                 <head>
                     <meta charSet="utf-8" />
                     <title>{this.props.title}</title>
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+                    <meta name="renderer" content="webkit" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui" />
                 </head>
                 <body>
                     {this.props.children}
