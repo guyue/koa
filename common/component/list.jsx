b2ce1e9f54813a286ada6218597f07fb40dc7ece
GuYue <guyue.jiwei@gmail.com>
2016-01-10 00:31:26 +0800

    重构 list.jsx，使其更健壮，并通过 eslint 检测

--- common/component/list.jsx
+++ common/component/list.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 import React, {
     Component,
     PropTypes,
 } from 'react';
+/* eslint-enable no-unused-vars */
 
 import Item from './item.jsx';
 
@@ -10,18 +12,17 @@ export default class List extends Component {
     render() {
         const prize = this.props.prize;
         const raffled = this.props.raffled;
+
         return (
             <div>
                 <ul className="list">
-                    {raffled.map((user) => {
-                        return (
-                            <Item
-                                key={user.image}
-                                user={user}
-                                removeRaffled={this.props.removeRaffled}
-                            />
-                        );
-                    })}
+                    {raffled.map((user) => (
+                        <Item
+                            key={user.image}
+                            user={user}
+                            removeRaffled={this.props.removeRaffled}
+                        />
+                    ))}
                 </ul>
                 <code>{raffled.length} / {prize.total}</code>
             </div>
@@ -29,3 +30,19 @@ export default class List extends Component {
     }
 
 }
+
+List.propTypes = {
+    prize: PropTypes.shape({
+        key: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        total: PropTypes.number.isRequired,
+        parallel: PropTypes.number,
+    }).isRequired,
+    raffled: PropTypes.arrayOf(PropTypes.shape({
+        department: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        phone: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        prize: PropTypes.string.isRequired,
+    })),
+};
