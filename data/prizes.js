ff7a9aa5d1724deadb3a94f41e377978847057af
GuYue <guyue.jiwei@gmail.com>
2016-01-11 20:28:03 +0800

    调整奖品的数量

--- data/prizes.js
+++ data/prizes.js
@@ -1,18 +1,18 @@
 module.exports = [{
         key: 'grateful',
         name: '感恩奖',
-        total: 30,
-        parallel: 5,
+        total: 100,
+        parallel: 8,
     }, {
         key: 'fifth',
         name: '五等奖',
-        total: 20,
-        parallel: 4,
+        total: 30,
+        parallel: 6,
     }, {
         key: 'fourth',
         name: '四等奖',
-        total: 10,
-        parallel: 2,
+        total: 15,
+        parallel: 5,
     }, {
         key: 'third',
         name: '三等奖',
@@ -20,7 +20,7 @@ module.exports = [{
     }, {
         key: 'second',
         name: '二等奖',
-        total: 3,
+        total: 2,
     }, {
         key: 'first',
         name: '一等奖',
