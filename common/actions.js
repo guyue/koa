f1157782cb6ffefe2603f7617ad076000a60aa1b
GuYue <guyue.jiwei@gmail.com>
2016-01-12 19:07:15 +0800

    添加actions注释

--- common/actions.js
+++ common/actions.js
@@ -4,6 +4,16 @@ import {
 
 import storage from '../static/src/js/storage.js';
 
+/**
+ * 生成glance action
+ * @param {Array} payload 当前摇奖器要显示的用户信息
+ * @return {Object} glance action
+ * @example
+ * glance([{
+ *     index: 1,
+ *     user: {},
+ * },])
+ */
 export function glance(payload) {
     return {
         type: ActionConstants.GLANCE,
@@ -11,6 +21,18 @@ export function glance(payload) {
     };
 }
 
+/**
+ * 生成raffle action
+ * @param {Array} payload 当前摇奖器要显示的中奖用户信息
+ * @return {Object} raffle action
+ * @example
+ * raffle([{
+ *     index: 1,
+ *     user: {
+ *         prize: 'prize.key',
+ *     }
+ * }])
+ */
 export function raffle(payload) {
     payload.forEach((message) => {
         storage.append(message.user);
@@ -21,6 +43,11 @@ export function raffle(payload) {
     };
 }
 
+/**
+ * 生成changePrize action
+ * @param {Number} index 更改奖项索引
+ * @return {Object} changePrize action
+ */
 export function changePrize(index) {
     return {
         type: ActionConstants.CHANGE_PRIZE,
@@ -28,14 +55,23 @@ export function changePrize(index) {
     };
 }
 
+/**
+ * 生成removeRaffled action
+ * @param {user} user 要删除的用户信息
+ * @return {Object} removeRaffled action
+ */
 export function removeRaffled(user) {
     storage.remove(user);
     return {
         type: ActionConstants.REMOVE_RAFFLED,
-        user: user,
+        user,
     };
 }
 
+/**
+ * 生成clearAll action
+ * @return {Object} clearAll action
+ */
 export function clearAll() {
     storage.clear();
     return {
@@ -43,6 +79,10 @@ export function clearAll() {
     };
 }
 
+/**
+ * 生成initUsers action
+ * @return {Object} initUsers action
+ */
 export function initUsers() {
     return {
         type: ActionConstants.INIT_USERS,
