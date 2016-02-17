b35bc820a11286960909266c41ff9b754b298790
GuYue <guyue.jiwei@gmail.com>
2016-02-17 11:20:58 +0800

    拆分user视图

---
+++ common/container/user.jsx
@@ -0,0 +1,66 @@
+import React, {
+    Component,
+    PropTypes,
+} from 'react';
+
+
+export default class User extends Component {
+
+    render() {
+
+        return (
+            <div>
+                <link rel="stylesheet" href="/less/user.css" />
+                <div className="container">
+                    <table className="table table-bordered table-hover">
+                        <caption>{this.props.title}</caption>
+                        <thead>
+                            <tr>
+                                <th>序号</th>
+                                <th>部门</th>
+                                <th>姓名</th>
+                                <th>手机号</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.props.users.map((user, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{user.department}</td>
+                                        <td>
+                                            <a
+                                                href={user.image}
+                                                target="_blank"
+                                                data-toggle="tooltip"
+                                            >
+                                                    <img src={user.image} className="img-thumbnail" />
+                                            </a>
+                                            {user.name}
+                                        </td>
+                                        <td>{user.phone}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+                <script src="/lib/jquery.js"></script>
+                <script src="/lib/bootstrap.js"></script>
+                <script src="/js/user.js"></script>
+            </div>
+        );
+
+    }
+
+}
+
+User.propTypes = {
+    title: PropTypes.string.isRequired,
+    users: PropTypes.arrayOf(PropTypes.shape({
+        department: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        phone: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+    }).isRequired).isRequired,
+};
--- view/user.jsx
+++ view/user.jsx
@@ -4,49 +4,16 @@ import React, {
 } from 'react';
 
 import Layout from './layout';
+import User from '../common/container/user';
 
 
-export default class User extends Component {
+export default class Index extends Component {
 
     render() {
 
         return (
             <Layout title={this.props.title}>
-                <link rel="stylesheet" href="/less/user.css" />
-                <div className="container">
-                    <table className="table table-bordered table-hover">
-                        <caption>{this.props.title}</caption>
-                        <thead>
-                            <tr>
-                                <th>序号</th>
-                                <th>部门</th>
-                                <th>姓名</th>
-                                <th>手机号</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.props.users.map((user, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{index + 1}</td>
-                                        <td>{user.department}</td>
-                                        <td>
-                                            <a
-                                                href={user.image}
-                                                target="_blank"
-                                                data-toggle="tooltip"
-                                            >
-                                                    <img src={user.image} className="img-thumbnail" />
-                                            </a>
-                                            {user.name}
-                                        </td>
-                                        <td>{user.phone}</td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </div>
+                <User {...this.props }/>
                 <script src="/lib/jquery.js"></script>
                 <script src="/lib/bootstrap.js"></script>
                 <script src="/js/user.js"></script>
@@ -57,7 +24,7 @@ export default class User extends Component {
 
 }
 
-User.propTypes = {
+Index.propTypes = {
     title: PropTypes.string.isRequired,
     users: PropTypes.arrayOf(PropTypes.shape({
         department: PropTypes.string.isRequired,
